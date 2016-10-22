030f3b9c94251d63212d45a9ada20cf7e438e768
Joseph Weigl <joseph.weigl@gmail.com>
2016-10-22 16:15:08 +0200

    Add ubuntu.zsh

---
+++ ubuntu.zsh
@@ -0,0 +1,11 @@
+# Ubuntu specific dotfile for zsh
+
+# default aliases
+alias editubuntu='nano ~/dotfiles/ubuntu.zsh && source ~/dotfiles/ubuntu.zsh'
+
+## Apt aliases
+alias aptinst='sudo apt-get install'
+alias aptupdate='sudo apt-get update'
+alias aptupgrade='sudo apt-get upgrade'
+alias aptdistupgrade='sudo apt-get dist-upgrade'
+alias aptremove='sudo apt-get remove'
