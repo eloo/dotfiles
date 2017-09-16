ce0f6af6133ff59890a11eabf24a3dfded339d2c
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2017-09-16 17:57:48 +0200

    Add official sublime repo

--- scripts_ubuntu/init_ubuntu.sh
+++ scripts_ubuntu/init_ubuntu.sh
@@ -40,7 +40,8 @@ sudo add-apt-repository ppa:gophers/archive
 sudo add-apt-repository ppa:webupd8team/atom
 
 # Sublime Text3
-sudo add-apt-repository ppa:webupd8team/sublime-text-3
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 # Oracle java installer
 sudo add-apt-repository ppa:webupd8team/java
