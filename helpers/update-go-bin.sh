d939a5115b5c7b20c27623020c05bb9b69157a69
Joseph Weigl <fents@gmx.net>
2017-11-18 22:45:16 +0100

    Update to go 1.9

--- helpers/update-go-bin.sh
+++ helpers/update-go-bin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for bin in /usr/lib/go-1.8/bin/* ; do
+for bin in /usr/lib/go-1.9/bin/* ; do
     sudo update-alternatives --install /usr/bin/$(basename $bin) $(basename $bin) $bin 0
     sudo update-alternatives --set $(basename $bin) $bin
 done
