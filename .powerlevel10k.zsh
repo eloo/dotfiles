1097bf63e404f79e677142524921814d32af44ac
Joseph Weigl <joseph.weigl@gmail.com>
2020-06-17 18:14:07 +0200

    Adjust theme

--- .powerlevel10k.zsh
+++ .powerlevel10k.zsh
@@ -1,11 +1,15 @@
 # Additional powerlevel10k configuration
-typeset -g -a POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=( os_icon dir vcs )
+typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(status os_icon context dir dir_writable vcs)
 typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(command_execution_time time)
 typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
 typeset -g POWERLEVEL9K_COMMAND_EXECUTION_TIME_THRESHOLD=2
 typeset -g POWERLEVEL9K_DIR_MAX_LENGTH=20
 typeset -g POWERLEVEL9K_TRANSIENT_PROMPT=false
 typeset -g POWERLEVEL9K_MODE='awesome-patched'
+typeset -g POWERLEVEL9K_STATUS_OK=false
+typeset -g POWERLEVEL9K_STATUS_VERBOSE=false
+typeset -g POWERLEVEL9K_STATUS_ERROR_FOREGROUND='red'
+
 # typeset -g POWERLEVEL9K_MODE=nerdfont-complete
 # typeset -g POWERLEVEL9K_VCS_BRANCH_ICON=""
 
@@ -21,6 +25,5 @@ typeset -g POWERLEVEL9K_MODE='awesome-patched'
 # typeset -g POWERLEVEL9K_VCS_OUTGOING_CHANGES_ICON='\u2191'
 # typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND='yellow'
 # typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND='cyan'
-# typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(status os_icon context dir dir_writable vcs)
-# typeset -g POWERLEVEL9K_STATUS_VERBOSE=false
+
 # typeset -g POWERLEVEL9K_TIME_FORMAT="%D{%H:%M}"
