26891af73edda8843d864290f0c5c9c68d0afde3
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2021-10-11 21:57:31 +0200

    Update .install.zsh

--- .install.zsh
+++ .install.zsh
@@ -24,13 +24,14 @@ function install_macos(){
 }
 
 function install_debian(){
+    sudo apt install curl
+    curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
+    
     arch=$(dpkg --print-architecture)
-    if [[ $arch == 'amd64' ]]; then 
-        curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
-        wget https://github.com/gsamokovarov/jump/releases/download/v0.30.1/jump_0.30.1_amd64.deb && sudo dpkg -i jump_0.30.1_amd64.deb && rm jump_0.30.1_amd64.deb
+    if [[ $arch == 'amd64' ]]; then     
+        wget https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_0.40.0_amd64.deb && sudo dpkg -i jump_0.40.0_amd64.deb && rm jump_0.40.0_amd64.deb
     fi
     if [[ $arch == 'armhf' ]]; then 
-        curl -sfL git.io/antibody | sudo sh -s - -b /usr/local/bin
         wget https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_linux_arm_binary && sudo mv jump_linux_arm_binary /usr/bin/jump && chmod +x /usr/bin/jump
     fi
 }
