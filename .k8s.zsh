9a26ffb9f99612976a8e5891c75a5cf3a88f57f1
Joseph Weigl <joseph.weigl@gmail.com>
2023-02-24 13:33:25 +0100

    Update plugins

--- .k8s.zsh
+++ .k8s.zsh
@@ -1,1 +1,5 @@
 alias crossplane-unsynced="k get managed -o=jsonpath='{.items[?(@.status.conditions[1].status==\"False\")]}' | jq --raw-output '[.kind, .metadata.name, .status.conditions[1].message] | @tsv'"
+
+antibody bundle ohmyzsh/ohmyzsh path:plugins/kubectl >>~/.zsh_plugins.zsh
+
+compinit
