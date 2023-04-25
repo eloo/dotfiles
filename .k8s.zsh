20dbf0b126760dab536fa4341183bdd7c18274e1
Joseph Weigl <joseph.weigl@gmail.com>
2023-04-25 16:25:38 +0200

    Add ky alias for kubectl yaml

--- .k8s.zsh
+++ .k8s.zsh
@@ -1,4 +1,9 @@
+
+### Crossplane
 alias crossplane-unsynced="k get managed -o=jsonpath='{.items[?(@.status.conditions[1].status==\"False\")]}' | jq --raw-output '[.kind, .metadata.name, .status.conditions[1].message] | @tsv'"
+
+### Kubernetes
+alias ky="kubectl -o yaml"
 
 antibody bundle ohmyzsh/ohmyzsh path:plugins/kubectl >>~/.zsh_plugins.zsh
 
